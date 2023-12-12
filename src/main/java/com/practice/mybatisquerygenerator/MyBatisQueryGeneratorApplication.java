f2f3533f274f3ef349d57530a58a1ad1b28c7c36
Reading-Snail <readingsnail17@gmail.com>
2023-12-12 09:32:41 +0900

    first commit

---
+++ src/main/java/com/practice/mybatisquerygenerator/MyBatisQueryGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.practice.mybatisquerygenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBatisQueryGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBatisQueryGeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/practice/mybatisquerygenerator/MyBatisQueryGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.mybatisquerygenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBatisQueryGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
